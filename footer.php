cce08861c77624d0c451605e8f2b6fede60a1fbb
LuckyLuckoso <70530022+LuckyLuckoso@users.noreply.github.com>
2021-04-26 22:10:34 -0300

    Template base

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer id="footer">
+	<p class="copyright">&copy; Sem titulo. All rights reserved. Demo Images: <a href="https://unsplash.com">Unsplash</a>. Design: <a href="https://html5up.net">HTML5 UP</a>.</p>
+</footer>
+
+</div>
+</div>
+
+</div>
---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE HTML>
+<!--
+	Editorial by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+<head>
+	<title>Editorial by HTML5 UP</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+	<link rel="stylesheet" href="assets/css/main.css" />
+</head>
+<body class="is-preload">
+
+	<!-- Wrapper -->
+				<?php
+				include 'wrapper.php';
+				?>
+
+		<!-- Main -->
+				<?php
+				include 'main.php';
+				?>
+
+				<!-- Header -->
+				<?php
+				include 'header.php';
+				?>
+
+				<!-- Banner -->
+				<?php
+				include 'banner.Php';
+				?>	
+
+				<!-- Section -->
+				<?php
+				include 'include1.php';
+				?>
+
+
+
+				<!-- Sidebar -->
+				<?php
+				include 'sidebar.php';
+				?>
+				<!-- Search -->
+				<?php
+				include 'search.php';
+				?>
+
+				<!-- Menu -->
+				<?php
+				include 'menu.php';
+				?>								
+
+				<!-- Section -->
+				<?php
+				include 'sectiond.php';
+				?>
+
+				<!-- Section -->
+				<?php
+				include 'sectiont.php'
+				?>
+
+				<!-- Footer -->
+				<?php
+				include 'footer.php'
+				?>
+				<!-- Scripts -->
+				<script src="assets/js/jquery.min.js"></script>
+				<script src="assets/js/browser.min.js"></script>
+				<script src="assets/js/breakpoints.min.js"></script>
+				<script src="assets/js/util.js"></script>
+				<script src="assets/js/main.js"></script>
+
+			</body>
+			</html>
